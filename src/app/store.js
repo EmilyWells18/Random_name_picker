c1f71620651912e0cfd393ec713a598108c81082
ewells <ewells@magicseaweed.com>
2021-04-16 16:14:32 +0100

    changes to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,43 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, createSelector, createStore } from '@reduxjs/toolkit';
 import nameReducer from '../features/picker/namePicker';
 
-export default configureStore({
-  reducer: {
+function loadFromLocalStorage() {
+  try{
+    const serializedState = localStorage.getItem('state');
+    if(serializedState === null){
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  }catch(err){
+    return undefined;
+  }
+}
+
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState)
+  } catch(err){
+    console.log(err);
+  }
+}
+
+const rootReducer = combineReducers({
     name: nameReducer,
-  },
 });
+
+const persistedState = loadFromLocalStorage();
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  window.__REDUX_DEV_TOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(()=> saveToLocalStorage(store.getState()));
+export default store;
+// export default configureStore({
+//   reducer: {
+//     name: nameReducer,
+//   },
+// });
